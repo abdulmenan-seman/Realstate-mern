41068b862975e4b6cbbdd7ff0511481b1fd10c49
abdulmenan-seman <abdulmenanseman0@gmail.com>
2026-04-26 06:55:29 +0300

    create middllware and a function to handel possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/User.model.js";
 import bcrypt from "bcryptjs";
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
    const newUser = new User({
@@ -11,7 +11,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json({ message: "User created successfully" });
     } catch (error) {
-        res.status(500).json({ message: "Error creating user", error: error.message });
+        next(error);
     }
 
 };
